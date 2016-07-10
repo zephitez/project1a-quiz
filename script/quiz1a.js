41ac7b14c0e6386716c0eaa2f026c77a9baac6ca
Zhezhang Fu <zephitez@gmail.com>
2016-07-10 21:02:56 +0800

    layout completed

---
+++ script/quiz1a.js
@@ -0,0 +1,46 @@
+function Questions (question, correctAns) {
+  this.question = question;
+  this.correctAns = correctAns;
+}
+
+var Question1 = new Questions ('Shiok means to fall in love with a cat', 1);
+var Question2 = new Questions ('Shiok means to fall in love with a cat', 0);
+var Question3 = new Questions ('Shiok means to fall in love with a cat', 1);
+var Question4 = new Questions ('Shiok means to fall in love with a cat', 0);
+var Question5 = new Questions ('Shiok means to fall in love with a cat', 1);
+var Question6 = new Questions ('Shiok means to fall in love with a cat', 0);
+var Question7 = new Questions ('Shiok means to fall in love with a cat', 0);
+var Question8 = new Questions ('Shiok means to fall in love with a cat', 1);
+var Question9 = new Questions ('Shiok means to fall in love with a cat', 1);
+var Question10 = new Questions ('Shiok means to fall in love with a cat', 0);
+
+
+// numberOfQuestions()
+//return an integer showing no of qns
+
+// currentQuestion()
+//return an integer that is the zero-based index of the current question
+
+// correctAnswer()
+//return an integer that is the zero-based index the correct answer for the currrent question
+
+// numberOfAnswers()
+//return an integer that is the number of choices for the current question
+
+// playTurn(choice)
+// It should take a single integer, which specifies which choice the current player wants to make. It should return a boolean true/false if the answer is correct.
+
+// isGameOver()
+// return a true or false
+
+// whoWon()
+// return 0 if not finished. Else return  1 or 2 depending on who won. return 3 if draw.
+
+// restart()
+// restart game
+
+//updateScore()
+
+$(document).ready(function() {
+
+});
